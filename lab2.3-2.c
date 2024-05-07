7be5ae50af4087e95f779f71f2d3a201d5ce0c20
irishN24 <145177386+irishN24@users.noreply.github.com>
2024-05-07 21:28:43 +0700

    Add files via upload

---
+++ lab2.3-2.c
@@ -0,0 +1,35 @@
+//Протабулировать функцию y = cos x  в интервале от а до b с шагом h
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double Y, a, b, h, y;
+    printf("Input a, b, h:\n");
+    int rez = scanf_s("%lf%lf%lf", &a, &b, &h);
+    double x;
+
+    if (rez == 3)
+    {
+        if (a == b) {
+            printf("A = B!!!");
+        }
+        if (a < b) {
+            if (h >= a && h <= b) {
+                for (x = a; x < b; x += h) {
+                    Y = cos(x);
+                    printf("x: %lf,Y: %lf\n", x, Y);
+                }
+            }
+            else {
+                printf("Function values not found!");
+            }
+        }
+        
+        //printf("x: %lf,Y: %lf\n", x, Y);
+    }
+    else
+    {
+        printf("Wrong Data");
+    }
+    return 0;
+}
