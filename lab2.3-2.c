d0074ea62b49773513111b708ea237cf5077b0c4
irishN24 <145177386+irishN24@users.noreply.github.com>
2024-05-08 16:14:52 +0700

    Update lab2.3-2.c

--- lab2.3-2.c
+++ lab2.3-2.c
@@ -2,29 +2,41 @@
 #include <math.h>
 int main()
 {
-    double Y, a, b, h, y;
+    double Y, a, b, h, x;
     printf("Input a, b, h:\n");
     int rez = scanf_s("%lf%lf%lf", &a, &b, &h);
-    double x;
 
     if (rez == 3)
     {
         if (a == b) {
-            printf("A = B!!!");
+            Y = cos(a);
+            printf("x: %lf,Y: %lf\n", a, Y);
         }
         if (a < b) {
             if (h >= a && h <= b) {
-                for (x = a; x < b; x += h) {
+                for (x = a; ((x >= a) && (x < b)); x += h) {
                     Y = cos(x);
                     printf("x: %lf,Y: %lf\n", x, Y);
                 }
             }
-            else {
-                printf("Function values not found!");
+            if ((h < a) || (h > b)) {
+                Y = cos(a);
+                printf("x: %lf,Y: %lf\n", a, Y);
             }
         }
-        
-        //printf("x: %lf,Y: %lf\n", x, Y);
+        if (a > b) {
+            if (h <= a && h >= b) {
+                for (x = a; ((x <= a) && (x > b)); x += h) {
+                    Y = cos(x);
+                    printf("x: %lf,Y: %lf\n", x, Y);
+                }
+            }
+            if ((h > a) || (h < b)) {
+                Y = cos(a);
+                printf("x: %lf,Y: %lf\n", a, Y);
+            }
+        }
+
     }
     else
     {
